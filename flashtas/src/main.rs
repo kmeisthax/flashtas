745db11471e79341ce41e4458428ca357b35ded5
David Wendt <dcrkid@yahoo.com>
2022-02-27 00:09:01 -0500

    Don't use the "really registration-free COM" trick to load COM classes straight from a DLL, since Flash Player doesn't like that.
    Yes, this means people who use FlashTAS will need to have an actually working Flash Player (and not just a bare `OCX`).

--- flashtas/src/main.rs
+++ flashtas/src/main.rs
@@ -1,20 +1,15 @@
 use activex_rs::bindings::flash::{IShockwaveFlash, SHOCKWAVE_FLASH_CLSID};
-use activex_rs::get_class_object_by_dll;
-use com::interfaces::IClassFactory;
-use com::runtime::init_runtime;
-use std::env::args;
-use std::ffi::OsStr;
+use com::runtime::{create_instance, init_runtime};
 
 fn main() {
     init_runtime().expect("A working COM runtime");
 
-    let path = args().nth(1).expect("Path to .exe, .dll, or .ocx");
-    let fp_ocx_name = OsStr::new(&path);
-    let fp_class = get_class_object_by_dll::<IClassFactory>(fp_ocx_name, &SHOCKWAVE_FLASH_CLSID)
-        .expect("ShockwaveFlash class");
-    let _fp = fp_class
-        .create_instance::<IShockwaveFlash>()
-        .expect("A working FlashPlayer");
+    let fp = create_instance::<IShockwaveFlash>(&SHOCKWAVE_FLASH_CLSID).expect("Flash");
 
-    //Do something else, presumably.
+    unsafe { fp.Zoom(1).expect("No whammies no whammies no whammies") };
+
+    println!(
+        "Flash version: {}",
+        unsafe { fp.FlashVersion() }.expect("Flash version")
+    );
 }
